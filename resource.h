c973aec20b21145b252773c20bdf7b1f4aba9668
Garuya <keicoon15@gmail.com>
2014-04-29 22:45:50 +0900

    Chatting&FileTransfer
    #2 DataCommunication

---
+++ resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ipc.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_IPC_DIALOG                  102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT_MSG                    1000
+#define IDC_BUTTON_SEND                 1001
+#define IDC_LIST_CHAT                   1003
+#define IDC_EDIT_SRC                    1004
+#define IDC_EDIT_DST                    1005
+//#define IDC_CHECK_TOALL                 1006
+#define IDC_BUTTON_ADDR                 1007
+//#define IDC_EDIT_STATE                  1008
+//#define IDC_EDIT1                       1009
+#define IDC_EDIT_FILESEARCH             1009
+//#define IDC_BUTTON1                     1010
+#define IDC_BUTTON_FILESERCH            1010
+#define IDC_BUTTON_FILESEARCH           1010
+//#define IDC_EDIT2                       1011
+//#define IDC_BUTTON2                     1012
+#define IDC_BUTTON_FILETRANS            1012
+//#define IDC_COMBO1                      1013
+#define IDC_COMBO_NIC                   1013
+#define IDC_STATI                       1014
+//#define IDC_BUTTON3                     1015
+//#define IDC_BUTTON_Test_Packet_Send     1015
+#define IDC_TEST_PACKET                 1015
+//#define IDC_BUTTON_RECEIVE              1016
+#define IDC_PROGRESS                    1017
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1018
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
